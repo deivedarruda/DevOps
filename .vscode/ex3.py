0980e75ef1f7a9ac9dc34931ccabd0dc093286f6
deivedarruda <deived.a.s@hotmail.com>
2017-09-21 09:34:35 -0300

    atualizando

---
+++ .vscode/ex3.py
@@ -0,0 +1,8 @@
+dias_semana = ("segunda", "teça", "quarta", "quinta", "sexta")
+
+print(type(dias_semana))
+print(dias_semana)
+
+print(dias_semana[3])
+for i in range(len(dias_semana)):
+    print(dias_semana[i])
