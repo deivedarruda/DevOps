14c9d13b269621e3fab78f29dae5f3be3ce0c5df
deivedarruda <deived.a.s@hotmail.com>
2017-09-21 10:41:16 -0300

    atualizando

--- .vscode/ex3.py
+++ .vscode/ex3.py
@@ -8,3 +8,11 @@ print(dias_semana[3])
 print()
 for i in range(len(dias_semana)):
     print(dias_semana[i])
+
+from Exercício import minutos
+
+R, h = minutos(125)
+print("%dh %dm"%(R,h))
+print()
+R, h = minutos(249)
+print("%dh %dm"%(R,h))
